7be4f8af5c99161616d3a2e53b189aea2e29ca25
apophatique <gaziz.sattarov@gmail.com>
2021-05-02 18:41:53 +0600

    now student have a face feature field

--- recognition-system/database/student.py
+++ recognition-system/database/student.py
@@ -1,13 +1,17 @@
 class Student:
-    def __init__(self, name, face_image):
+    def __init__(self, name, face_image, face_encoding):
         self.name = name
         self.face_image = face_image
+        self.face_encoding = face_encoding
 
     def get_face(self):
         return self.face_image
 
     def get_name(self):
         return self.name
+
+    def get_encoding(self):
+        return self.face_encoding
 
     def __str__(self):
         return self.name
--- recognition-system/database/student_database.py
+++ recognition-system/database/student_database.py
@@ -1,6 +1,9 @@
 import os
 import cv2
+import face_recognition
+
 from student import Student
+
 
 class Database:
     def __init__(self):
@@ -8,9 +11,14 @@ class Database:
 
     def get_database(self, path):
         student_database = []
+
         for filename in os.listdir(path):
-            new_student = Student(filename.split('.')[0],
-                                  cv2.imread(path + '/' + filename, cv2.IMREAD_COLOR))
+            face_image = cv2.imread(path + '/' + filename, cv2.IMREAD_COLOR)
+            new_student = Student(
+                filename.split('.')[0],
+                face_image,
+                face_recognition.face_encodings(face_image)[0]
+            )
             student_database.append(new_student)
 
         return student_database
