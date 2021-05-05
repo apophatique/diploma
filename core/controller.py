a4b001b9414e696236bc6c247150e677bfe019ca
apophatique <gaziz.sattarov@gmail.com>
2021-05-05 15:37:42 +0600

    updated method for a correctly results sctructure

--- core/controller.py
+++ core/controller.py
@@ -1,26 +1,30 @@
 from kernel import RecognitionSystem
 from work_states import SystemWorkStates
 from student_database import Database
-from schedule.schedule import ScheduleController
 from IO import IO
 
 config = {
     'recognition_threshold': 0.1,
     'save_frame': True,
     'save_results': True,
-    'frame_output_path': '../output/frame.jpg',
-    'results_output_path': '../output/test1_detect_res.txt'
+    'frame_output_path': 'output/frame.jpg',
+    'results_output_path': 'output/results.csv',
+    'faces_detected': 'output/faces_detected/'
 }
+
 
 class Controller:
     def __init__(self):
-        self.system_work_state = SystemWorkStates.webcam
         self.recognition_system = RecognitionSystem()
         self.database = Database()
-        self.database_path = '../input/database/popular/'
+        self.database_path = 'input/database/'
 
-    def run(self):
+    def run(self, system_work_state):
         database = self.database.get_database(self.database_path)
-        results, frame, boxes = self.recognition_system.run(self.system_work_state, database)
+        results, frame = self.recognition_system.run(system_work_state, database)
 
-        IO.write_csv_results(results)
+        IO.write_csv_results(results, config['results_output_path'])
+
+        if system_work_state is SystemWorkStates.image:
+            IO.save_image(frame, config['frame_output_path'])
+            IO.save_detected_faces(results, frame, config['faces_detected'])
