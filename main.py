2e5c3330b2be24f56be54bc088bd6b23d07f7e8d
apophatique <gaziz.sattarov@gmail.com>
2021-05-05 15:39:16 +0600

    arguments for work modes added

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import argparse
 from controller import Controller
+from work_states import SystemWorkStates
+
+work_modes = {
+    'image': SystemWorkStates.image,
+    'webcam': SystemWorkStates.webcam,
+    'video': SystemWorkStates.video
+}
 
 
 def main():
+    parser = argparse.ArgumentParser(description='take a picture')
+    parser.add_argument('-mode', '-n', default='unknown', type=str, help='input the name of the recording person')
+    args = parser.parse_args()
     controller = Controller()
-    controller.run()
+    controller.run(work_modes[args.mode])
 
 
 if __name__ == '__main__':
