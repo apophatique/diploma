92bdabbc21e5c03644bcc4ab41904dbdcbcd18b2
apophatique <gaziz.sattarov@gmail.com>
2021-05-09 10:13:27 +0600

    work_modes removed, arguments parsed out of method

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import argparse
 from controller import Controller
-from work_states import SystemWorkStates
 
-work_modes = {
-    'image': SystemWorkStates.image,
-    'webcam': SystemWorkStates.webcam,
-    'video': SystemWorkStates.video
-}
+parser = argparse.ArgumentParser()
+parser.add_argument('-mode', default='webcam', type=str)
+parser.add_argument('-auditorium', type=str)
+parser.add_argument('-date', type=str)
+args = parser.parse_args()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='take a picture')
-    parser.add_argument('-mode', '-n', default='unknown', type=str, help='input the name of the recording person')
-    args = parser.parse_args()
     controller = Controller()
-    controller.run(work_modes[args.mode])
+    controller.run(
+        args.mode
+    )
 
 
 if __name__ == '__main__':
