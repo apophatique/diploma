103d8b90849b8a7d3a427a28eaf57aff23893631
apophatique <gaziz.sattarov@gmail.com>
2021-05-09 10:12:54 +0600

    config replaced in a new file

---
+++ util/config.py
@@ -0,0 +1,9 @@
+config = {
+    'faces_folder_path': 'input/database/',
+    'recognition_threshold': 0.1,
+    'save_frame': True,
+    'save_results': True,
+    'frame_output_path': 'output/frame.jpg',
+    'results_output_path': 'output/results.csv',
+    'faces_detected': 'output/faces_detected/'
+}
