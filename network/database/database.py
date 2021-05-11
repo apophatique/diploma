3cfb556fde82ba064c19594738f9622b0376fbb2
apophatique <gaziz.sattarov@gmail.com>
2021-05-11 13:16:01 +0600

    удалён метод создания списка студентов из папки, теперь работает полностью на sqlite

--- network/database/database.py
+++ network/database/database.py
@@ -8,65 +8,81 @@ from student_model import Student
 
 class Database:
     def __init__(self, path):
-        # self.create_db()
         self.path = path
-        # cv2.imread(path + '/' + filename, cv2.IMREAD_COLOR)
+        self.create_db()
 
     def create_db(self):
-        self.con = sl.connect('students.db')
-        cur = self.con.cursor()
-        create_db_query = '''
-            CREATE TABLE IF NOT EXISTS students (
-                name TEXT NOT NULL,
-                surname TEXT NOT NULL,
-                patronymic TEXT,
-                sex INTEGER NOT NULL,
-                group INTEGER NOT NULL,
-                photo BLOB NOT NULL
-            )
-        '''
-        # student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        with sl.connect('students.db') as con:
+            self.cur = con.cursor()
+            drop_table_query = '''
+                DROP TABLE IF EXISTS students
+            '''
+            self.cur.execute(drop_table_query)
 
-        self.con.execute(create_db_query)
+            create_db_query = '''
+                CREATE TABLE IF NOT EXISTS students (
+                    name TEXT NOT NULL,
+                    surname TEXT NOT NULL,
+                    patronymic TEXT,
+                    sex TEXT NOT NULL,
+                    [group] TEXT NOT NULL,
+                    face_image BLOB NOT NULL,
+                    face_encoding TEXT NOT NULL
+                )
+            '''
+            self.cur.execute(create_db_query)
 
-        insert_query = '''
-            INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?)
-        '''
-        for filename in os.listdir(self.path):
-            name, surname, patronymic, sex, group = filename.split('_')[0], \
-                                                    filename.split('_')[1], \
-                                                    filename.split('_')[2], \
-                                                    filename.split('_')[3], \
-                                                    filename.split('_')[4]
-            pict_binary = self.import_pict_binary(self.path + '/' + filename)
-            data = (name, surname, patronymic, sex, group, pict_binary)
-            self.con.execute(insert_query, data)
+            insert_query = '''
+                INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?)
+            '''
+            for filename in os.listdir(self.path):
+                img_path = self.path + filename
+                filename = filename.split('.')[0]
+                name, surname, patronymic, sex, group = filename.split('_')[0], \
+                                                        filename.split('_')[1], \
+                                                        filename.split('_')[2], \
+                                                        filename.split('_')[3], \
+                                                        filename.split('_')[4]
+                pict_binary = self.import_pict_binary(img_path)
+                print(img_path)
+                face_encoding = self.get_face_encoding(img_path)
+                face_encoding = sl.Binary(face_encoding)
+                data = (name, surname, patronymic, sex, group, pict_binary, face_encoding)
+                self.cur.execute(insert_query, data)
 
     def import_pict_binary(self, path):
         f = open(path, 'rb')
         pict_binary = f.read()
         return pict_binary
 
-    def get_students(self, group):
+    def get_face_encoding(self, path):
+        face = cv2.imread(path)
+        return face_recognition.face_encodings(face)[0]
+
+    def get_students_data(self, group):
         select_query = '''
-            SELECT * FROM students WHERE group == ?
+            SELECT * FROM students WHERE [group] in (?)
         '''
-        data = self.con.execute(select_query, group)
-        return data
+        if type(group) is list:
+            print('zxc: ', (group))
+            self.cur.execute(select_query, group)
+        else:
+            self.cur.execute(select_query, (group,))
+        items = self.cur.fetchall()
 
-    def get_database_from_folder(self):
-        student_database = []
+        students = []
+        for item in items:
+            students.append(Student(item))
+        return students
 
-        for filename in os.listdir(self.path):
-            face_image = cv2.imread(self.path + filename, cv2.IMREAD_COLOR)
-            new_student = Student(
-                filename.split('.')[0],  # Имя студента
-                face_image,  # Фото лица формата np.array
-                face_recognition.face_encodings(face_image)[0]  # Вектор, полученный из фото лица
-            )
-            student_database.append(new_student)
-        return student_database
+    def drop_row(self, group):
+        query = '''
+            DELETE FROM students WHERE [group] in ?
+        '''
+        self.cur.execute(query, (group, ))
 
 
 if __name__ == '__main__':
-    database = Database('input/database/')
+    group = ['PIN-171', 'FIT-211']
+    database = Database('D:\Projects\diploma/input/database/')
+
